a9366c0f1ada152531bc26d1fc2b201875c18f3a
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-09 01:14:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file, emoticon)
       end
     end
   end
-  return "sorry, don't know that one..."
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file, emoticon)
